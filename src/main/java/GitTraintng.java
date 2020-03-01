cfe328d680426c88ab62914123eac4a7d93203f6
Tomek <klips0303@gmail.com>
2020-03-01 12:15:02 +0100

    push

---
+++ src/main/java/GitTraintng.java
@@ -0,0 +1,10 @@
+/**
+ git -init   -tworzy repo
+ git commit m "wiadomość"
+ CTRL + k - tak wywołuje się commmity
+ **/
+final class GitTraintng {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
